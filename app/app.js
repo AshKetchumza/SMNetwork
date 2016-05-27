0ff0429937cafc243bad8821d71fdb59fadc21e9
Ashley Sanders <ashleymsanders2@gmail.com>
2016-05-27 22:04:55 +0200

    Added signup view and controller

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 (function(){
-  angular.module('SMNetwork', []);
+  angular.module('SMNetwork', ['ui.router'])
+      .config(function($stateProvider){
+
+        $stateProvider
+          .state('signUp', {
+          url: "/signup",
+          templateUrl: "app/signup/signup.html",
+          controller: "SignUpController"
+          })
+      })
 }());
---
+++ app/signup/signup-controller.js
@@ -0,0 +1,6 @@
+(function(){
+    angular.module('SMNetwork')
+    .controller('SignUpController', ['$scope', '$state', function($scope, $state){
+
+    }]);
+}());
