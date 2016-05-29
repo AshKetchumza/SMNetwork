e2206fc9542ac6778e4e1b6b681069e4bf707762
Ashley Sanders <ashleymsanders2@gmail.com>
2016-05-29 22:39:42 +0200

    Enables signup and login

--- app/signup/signup-controller.js
+++ app/signup/signup-controller.js
@@ -1,7 +1,16 @@
 (function(){
     angular.module('SMNetwork')
-    .controller('SignUpController', ['$scope', '$state',
-      function($scope, $state){
 
+    .controller('SignUpController', ['$scope', '$state', '$http',
+      function($scope, $state, $http){
+
+        $scope.createUser = function(){
+          console.log('$scope.newUser');
+          $http.post('api/user/signup', $scope.newUser).success(function(res){
+          }).error(function(error){
+            console.log(error);
+          })
+        }
     }]);
+
 }());
